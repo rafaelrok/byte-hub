057a5e135d8a1dfa91d079cc852ea5a004c12802
Rafael Vieira <rafaelrok25@gmail.com>
2024-05-22 12:01:18 -0300

    byte-hub: created project

---
+++ src/main/java/br/com/rafaelvieira/bytehub/ByteHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelvieira.bytehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteHubApplication.class, args);
+    }
+
+}
